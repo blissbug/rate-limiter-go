a9244a75fababd6a5c8eb367bc1e78b3bf35c33f
blissbug <4385anupriyasachan@gmail.com>
2026-05-02 23:48:33 +0530

    Fix formatting

--- fixedWindow.go
+++ fixedWindow.go
@@ -11,59 +11,58 @@ import (
 var mux sync.Mutex
 var fixedWindowRequests = make(map[string]windowData)
 
-type windowData struct{
-   count int 
-	 windowStart time.Time
+type windowData struct {
+	count       int
+	windowStart time.Time
 }
 
-type User struct{
+type User struct {
 	UserID string `json:"userID"`
 }
 
-//handles lock and unlock when reading/writing from dictionary 
+// handles lock and unlock when reading/writing from dictionary
 func FixedWindowRateLimiter(ctx *gin.Context) {
-	 user := new(User)
-	 err := ctx.ShouldBindJSON(&user)
-	 if err != nil{
-		 ctx.AbortWithStatusJSON(400,gin.H{"message":err})
-     return
-	 }
-
-	 fmt.Println(user)
-
-
-	 if user.UserID == ""{
-		 ctx.AbortWithStatusJSON(400,gin.H{"message":"user id not found!"})
-     return
-	 }
-
-	 mux.Lock()
-	 
-	 data,exists := fixedWindowRequests[user.UserID]
-
-	 if !exists{
-		 fixedWindowRequests[user.UserID] = windowData{1,time.Now()}
-		 mux.Unlock()
-		 ctx.Next()
-		 return
-	 }
-
-	 //time since last request was more than a minute
-	 if time.Since(data.windowStart) > 1 * time.Minute{
-      fixedWindowRequests[user.UserID] = windowData{1,time.Now()}
-			mux.Unlock()
-			ctx.Next()
-			return
-	 }
-	 //already reached max value
-	 if data.count >= 5{
-		mux.Unlock()
-		ctx.AbortWithStatusJSON(429, gin.H{"message":"blocked, too many calls in the last minute"})
+	user := new(User)
+	err := ctx.ShouldBindJSON(&user)
+	if err != nil {
+		ctx.AbortWithStatusJSON(400, gin.H{"message": err})
 		return
-	 }
-	 //update count
-	 data.count++
-	 fixedWindowRequests[user.UserID] = data
-	 mux.Unlock()
-	 ctx.Next()
+	}
+
+	fmt.Println(user)
+
+	if user.UserID == "" {
+		ctx.AbortWithStatusJSON(400, gin.H{"message": "user id not found!"})
+		return
+	}
+
+	mux.Lock()
+
+	data, exists := fixedWindowRequests[user.UserID]
+
+	if !exists {
+		fixedWindowRequests[user.UserID] = windowData{1, time.Now()}
+		mux.Unlock()
+		ctx.Next()
+		return
+	}
+
+	//time since last request was more than a minute
+	if time.Since(data.windowStart) > 1*time.Minute {
+		fixedWindowRequests[user.UserID] = windowData{1, time.Now()}
+		mux.Unlock()
+		ctx.Next()
+		return
+	}
+	//already reached max value
+	if data.count >= 5 {
+		mux.Unlock()
+		ctx.AbortWithStatusJSON(429, gin.H{"message": "blocked, too many calls in the last minute"})
+		return
+	}
+	//update count
+	data.count++
+	fixedWindowRequests[user.UserID] = data
+	mux.Unlock()
+	ctx.Next()
 }
--- slidingWindow.go
+++ slidingWindow.go
@@ -7,98 +7,98 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type slidingBucket struct{
-	count int 
-	slot int64
+type slidingBucket struct {
+	count int
+	slot  int64
 }
 
 var slidingWindowRequests = make(map[string][]slidingBucket) //all the requests for the users out there
 
-func getBucketSlot()int64{
-	slot := time.Now().Unix()/20
+func getBucketSlot() int64 {
+	slot := time.Now().Unix() / 20
 	return slot
 }
 
-//three windows inside a min 
-func slidingWindowLimiter(ctx *gin.Context){
-	 user := new(User)
-	 err := ctx.ShouldBindJSON(&user)
-	 if err != nil{
-		 ctx.AbortWithStatusJSON(400,gin.H{"message":err})
-     return
-	 }
+// three windows inside a min
+func slidingWindowLimiter(ctx *gin.Context) {
+	user := new(User)
+	err := ctx.ShouldBindJSON(&user)
+	if err != nil {
+		ctx.AbortWithStatusJSON(400, gin.H{"message": err})
+		return
+	}
 
-	 fmt.Println(user)
+	fmt.Println(user)
 
-	 if user.UserID == ""{
-		 ctx.AbortWithStatusJSON(400,gin.H{"message":"user id not found!"})
-     return
-	 }
+	if user.UserID == "" {
+		ctx.AbortWithStatusJSON(400, gin.H{"message": "user id not found!"})
+		return
+	}
 
-	 mux.Lock()
-	 data,exists := slidingWindowRequests[user.UserID]
-   bucketSlot := getBucketSlot()
-	 //initialize a full token bucket
-	 if !exists{
-		 bucketIndex := bucketSlot%3
-		 data = make([]slidingBucket, 3)
-		 data[bucketIndex]=slidingBucket{
-			count:1,
-			slot:bucketSlot,
-		 }
-		 slidingWindowRequests[user.UserID]=data
-		 mux.Unlock()
-		 ctx.Next()
-		 return
-	 }
+	mux.Lock()
+	data, exists := slidingWindowRequests[user.UserID]
+	bucketSlot := getBucketSlot()
+	//initialize a full token bucket
+	if !exists {
+		bucketIndex := bucketSlot % 3
+		data = make([]slidingBucket, 3)
+		data[bucketIndex] = slidingBucket{
+			count: 1,
+			slot:  bucketSlot,
+		}
+		slidingWindowRequests[user.UserID] = data
+		mux.Unlock()
+		ctx.Next()
+		return
+	}
 
-	 activeRequests := getActiveRequestsInBuckets(data,bucketSlot)
-   
-	if activeRequests >= 8{
-			mux.Unlock()
-			ctx.AbortWithStatusJSON(429,gin.H{"message":"too many requests in the last window"})
-			return
-	 }
+	activeRequests := getActiveRequestsInBuckets(data, bucketSlot)
 
-	 if activeRequests < 8{
+	if activeRequests >= 8 {
+		mux.Unlock()
+		ctx.AbortWithStatusJSON(429, gin.H{"message": "too many requests in the last window"})
+		return
+	}
+
+	if activeRequests < 8 {
 		//update count for the correct slot
-		 getUpdatedBuckets(data,bucketSlot)
-     slidingWindowRequests[user.UserID] = data
-		 mux.Unlock()
-		 ctx.Next()
-	 }
+		getUpdatedBuckets(data, bucketSlot)
+		slidingWindowRequests[user.UserID] = data
+		mux.Unlock()
+		ctx.Next()
+	}
 }
 
-func getActiveRequestsInBuckets(buckets []slidingBucket,bucketSlot int64)int{
+func getActiveRequestsInBuckets(buckets []slidingBucket, bucketSlot int64) int {
 	count := 0
-  for _,bucket := range buckets{
-     if bucket.isActive(bucketSlot){
-			count+=bucket.count
-		 }
+	for _, bucket := range buckets {
+		if bucket.isActive(bucketSlot) {
+			count += bucket.count
+		}
 	}
 	return count
 }
 
-func(b slidingBucket)isActive(bucketSlot int64)bool{
-   if bucketSlot - b.slot < 3 && bucketSlot - b.slot >= 0  {
+func (b slidingBucket) isActive(bucketSlot int64) bool {
+	if bucketSlot-b.slot < 3 && bucketSlot-b.slot >= 0 {
 		return true
-	 }
-	 return false
+	}
+	return false
 }
 
-//slices are reference types so they update
-func getUpdatedBuckets(buckets []slidingBucket,bucketSlot int64){
-	//dont do anything to inactive buckets 
-	bucketIndex := bucketSlot%3
+// slices are reference types so they update
+func getUpdatedBuckets(buckets []slidingBucket, bucketSlot int64) {
+	//dont do anything to inactive buckets
+	bucketIndex := bucketSlot % 3
 
 	//no such bucket exists with this slot so we create a new one
-	if buckets[bucketIndex].slot != bucketSlot{
+	if buckets[bucketIndex].slot != bucketSlot {
 		buckets[bucketIndex] = slidingBucket{
 			count: 0,
-			slot:bucketSlot,
+			slot:  bucketSlot,
 		}
 	}
-  if(buckets[bucketIndex].slot == bucketSlot){
-    	buckets[bucketIndex].count++;
+	if buckets[bucketIndex].slot == bucketSlot {
+		buckets[bucketIndex].count++
 	}
 }
